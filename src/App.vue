ee2c43454df8822b43b2de208f09eed33df074fe
mirzaak <mirzakaric002@gmail.com>
2022-01-23 01:32:51 +0100

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div class="navbar">
+<div class="left">
+<router-link to="/"><img src=".\assets\logo.svg" alt="#"></router-link>
+<div class="dropdown">
+<router-link to="/"><a href="#">Movies</a><img src="./assets/Vector.svg" alt="logo"></router-link>
+<div class="dropdowncontent">
+<router-link to="/"><a href="#">Popular</a></router-link>
+<router-link to="/"><a href="#">Now Playing</a></router-link>
+<router-link to="/"><a href="#">Upcoming</a></router-link>
+<router-link to="/"><a href="#">Top Rated</a></router-link>
+</div>
+</div>
+<div class="dropdown">
+<router-link to="/"><a href="#">TV Shows</a><img src="./assets/Vector.svg" alt="logo"></router-link>
+<div class="dropdowncontent">
+<router-link to="/"><a href="#">Popular</a></router-link>
+<router-link to="/"><a href="#">Airing Today</a></router-link>
+<router-link to="/"><a href="#">On TV</a></router-link>
+<router-link to="/"><a href="#">Top Rated</a></router-link>
+</div>
+<router-link to="/"><a href="#">People</a></router-link>
+</div>
+</div>
+
+<div class="right">
+<router-link to="/"><a href="#">Login</a></router-link>
+<router-link to="/"><a href="#">Join TMDB</a></router-link>
+<router-link to="/"><img src="./assets/search.svg" alt=""></router-link>
+</div>
+</div>
+
+<router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  font-family: 'Source Sans Pro', sans-serif;
+  margin: 0;
+}
+.navbar{
+  display: flex;
+  align-items: center;
+  height: 60px;
+  width: 100%;
+  background: #0d253f;
+  justify-content: space-between;
+}
+.navbar img{
+  width: 150px;
+}
+.right img{
+  width: 20px;
+}
+.navbar a{
+  text-decoration: none;
+  color: white;
+  margin-left: 20px;
+}
+.right{
+  display: flex;
+  margin-right: 100px;
+  align-items: center;
+}
+.left{
+  display: flex;
+  margin-left: 100px;
+
+}
+.dropdown img{
+  margin-left: 5px;
+  height: 15px;
+  width: 12px;
 }
 
-#nav {
-  padding: 30px;
+.dropdown:hover .dropdowncontent{
+  display: flex;
+}
+.dropdowncontent{
+  display: none;
+  flex-direction: column;
+  position: absolute;
+  text-align: left;
+  margin-left: 40px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  background: white;
+  position: absolute;
+}
+.dropdowncontent a{
+  padding-left: 10px;
+  padding-right: 20px;
+  margin-left: 0;
+  margin-right: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  color: #0d253f;
+  margin-top: 5px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
